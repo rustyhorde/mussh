babb427b32e9eca7a26bdbc6f3d8fdbb28cd6853
Jason Ozias <jason.ozias@kroger.com>
2018-07-29 22:31:38 -0400

    rustfmt

--- src/run.rs
+++ src/run.rs
@@ -30,14 +30,12 @@ pub fn run() -> Result<i32> {
                 .value_name("CONFIG")
                 .help("Specify a non-standard path for the TOML config file.")
                 .takes_value(true),
-        )
-        .arg(
+        ).arg(
             Arg::with_name("verbose")
                 .short("v")
                 .multiple(true)
                 .help("Set the output verbosity level (more v's = more verbose)"),
-        )
-        .subcommand(
+        ).subcommand(
             SubCommand::with_name("cmd")
                 .about("Work with 'cmd' configuration")
                 .subcommand(SubCommand::with_name("list").about("List the 'cmd' configuration"))
@@ -50,16 +48,14 @@ pub fn run() -> Result<i32> {
                                 .help("The assigned 'cmd' name")
                                 .index(1)
                                 .required(true),
-                        )
-                        .arg(
+                        ).arg(
                             Arg::with_name("cmd")
                                 .value_name("CMD")
                                 .help("The command string")
                                 .index(2)
                                 .required(true),
                         ),
-                )
-                .subcommand(
+                ).subcommand(
                     SubCommand::with_name("update")
                         .about("Update 'cmd' configuration")
                         .arg(
@@ -68,19 +64,16 @@ pub fn run() -> Result<i32> {
                                 .help("The assigned 'cmd' name")
                                 .index(1)
                                 .required(true),
-                        )
-                        .arg(
+                        ).arg(
                             Arg::with_name("cmd")
                                 .value_name("CMD")
                                 .help(
                                     "The command to update the \
                                      'cmd' with",
-                                )
-                                .index(2)
+                                ).index(2)
                                 .required(true),
                         ),
-                )
-                .subcommand(
+                ).subcommand(
                     SubCommand::with_name("remove")
                         .about("Remove a 'cmd' configuration")
                         .arg(
@@ -91,14 +84,12 @@ pub fn run() -> Result<i32> {
                                 .required(true),
                         ),
                 ),
-        )
-        .subcommand(
+        ).subcommand(
             SubCommand::with_name("hostlist")
                 .about("Work with 'hostlist' configuration")
                 .subcommand(
                     SubCommand::with_name("list").about("List the 'hostlist' configuration"),
-                )
-                .subcommand(
+                ).subcommand(
                     SubCommand::with_name("add")
                         .about("Add 'hostlist' configuration")
                         .arg(
@@ -107,20 +98,17 @@ pub fn run() -> Result<i32> {
                                 .help("The assigned 'hostlist' name")
                                 .index(1)
                                 .required(true),
-                        )
-                        .arg(
+                        ).arg(
                             Arg::with_name("hosts")
                                 .value_name("HOSTS")
                                 .multiple(true)
                                 .help(
                                     "The hosts to add to the \
                                      'hostlist'",
-                                )
-                                .index(2)
+                                ).index(2)
                                 .required(true),
                         ),
-                )
-                .subcommand(
+                ).subcommand(
                     SubCommand::with_name("update")
                         .about("Update 'hostlist' configuration")
                         .arg(
@@ -129,20 +117,17 @@ pub fn run() -> Result<i32> {
                                 .help("The assigned 'hosts' name")
                                 .index(1)
                                 .required(true),
-                        )
-                        .arg(
+                        ).arg(
                             Arg::with_name("hosts")
                                 .value_name("HOSTS")
                                 .multiple(true)
                                 .help(
                                     "The hosts to upate the \
                                      'hostlist' with",
-                                )
-                                .index(2)
+                                ).index(2)
                                 .required(true),
                         ),
-                )
-                .subcommand(
+                ).subcommand(
                     SubCommand::with_name("remove")
                         .about("Remove a 'hostlist' configuration")
                         .arg(
@@ -153,8 +138,7 @@ pub fn run() -> Result<i32> {
                                 .required(true),
                         ),
                 ),
-        )
-        .subcommand(
+        ).subcommand(
             SubCommand::with_name("hosts")
                 .about("Work with 'hosts' configuration")
                 .subcommand(SubCommand::with_name("list").about("List the 'hosts' configuration"))
@@ -167,36 +151,31 @@ pub fn run() -> Result<i32> {
                                 .help("The assigned 'hosts' name")
                                 .index(1)
                                 .required(true),
-                        )
-                        .arg(
+                        ).arg(
                             Arg::with_name("username")
                                 .value_name("USERNAME")
                                 .help("The username")
                                 .index(2)
                                 .required(true),
-                        )
-                        .arg(
+                        ).arg(
                             Arg::with_name("hostname")
                                 .value_name("HOSTNAME")
                                 .help("The hostname/ip address")
                                 .index(3)
                                 .required(true),
-                        )
-                        .arg(
+                        ).arg(
                             Arg::with_name("port")
                                 .value_name("PORT")
                                 .help("The port")
                                 .index(4)
                                 .required(true),
-                        )
-                        .arg(
+                        ).arg(
                             Arg::with_name("pem")
                                 .value_name("PEM")
                                 .help("A pem file path")
                                 .index(5),
                         ),
-                )
-                .subcommand(
+                ).subcommand(
                     SubCommand::with_name("update")
                         .about("Update 'hosts' configuration")
                         .arg(
@@ -206,39 +185,34 @@ pub fn run() -> Result<i32> {
                                 .value_name("USERNAME")
                                 .help("The updated username")
                                 .takes_value(true),
-                        )
-                        .arg(
+                        ).arg(
                             Arg::with_name("hostname")
                                 .short("h")
                                 .long("hostname")
                                 .value_name("hostname")
                                 .help("The updated hostname")
                                 .takes_value(true),
-                        )
-                        .arg(
+                        ).arg(
                             Arg::with_name("port")
                                 .short("p")
                                 .long("port")
                                 .value_name("PORT")
                                 .help("The updated port")
                                 .takes_value(true),
-                        )
-                        .arg(
+                        ).arg(
                             Arg::with_name("pem")
                                 .long("pem")
                                 .value_name("PEM")
                                 .help("The updated pem file path.")
                                 .takes_value(true),
-                        )
-                        .arg(
+                        ).arg(
                             Arg::with_name("name")
                                 .value_name("NAME")
                                 .help("The assigned 'hosts' name")
                                 .index(1)
                                 .required(true),
                         ),
-                )
-                .subcommand(
+                ).subcommand(
                     SubCommand::with_name("remove")
                         .about("Remove a 'hosts' configuration")
                         .arg(
@@ -249,26 +223,22 @@ pub fn run() -> Result<i32> {
                                 .required(true),
                         ),
                 ),
-        )
-        .subcommand(
+        ).subcommand(
             SubCommand::with_name("run")
                 .about("Run a command on hosts")
                 .arg(Arg::with_name("dry_run").long("dryrun").help(
                     "Parse config and setup the client, \
                      but don't run it.",
-                ))
-                .arg(Arg::with_name("sync").short("s").long("sync").help(
+                )).arg(Arg::with_name("sync").short("s").long("sync").help(
                     "Run the given commadn synchronously across the \
                      hosts.",
-                ))
-                .arg(
+                )).arg(
                     Arg::with_name("command")
                         .value_name("CMD")
                         .help("The command to multiplex")
                         .index(1)
                         .required(true),
-                )
-                .arg(
+                ).arg(
                     Arg::with_name("hosts")
                         .value_name("HOSTS")
                         .multiple(true)
@@ -276,8 +246,7 @@ pub fn run() -> Result<i32> {
                         .index(2)
                         .required(true),
                 ),
-        )
-        .get_matches();
+        ).get_matches();
 
     // Setup the logging
     let level = match matches.occurrences_of("verbose") {
